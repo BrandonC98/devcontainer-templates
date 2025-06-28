e1af4a80224414494a130a363602f2dbff9634bb
Brandon98 <brandoncampbell98@hotmail.co.uk>
2025-06-28 20:32:12 +0000

    clean up

--- .github/actions/smoke-test/test.sh
+++ .github/actions/smoke-test/test.sh
@@ -6,7 +6,6 @@ SRC_DIR="/tmp/${TEMPLATE_ID}"
 echo "Running Smoke Test"
 
 ID_LABEL="test-container=${TEMPLATE_ID}"
-devcontainer exec --workspace-folder "${SRC_DIR}" --id-label ${ID_LABEL} /bin/sh -c 'set -e && pwd && ls test-project/test.sh '
 devcontainer exec --workspace-folder "${SRC_DIR}" --id-label ${ID_LABEL} /bin/sh -c 'set -e && if [ -f "test-project/test.sh" ]; then cd test-project && if [ "$(id -u)" = "0" ]; then chmod +x test.sh; else sudo chmod +x test.sh; fi && ./test.sh; else ls -a; fi'
 
 # Clean up
--- test/neovim/test.sh
+++ test/neovim/test.sh
@@ -5,9 +5,9 @@ source test-utils.sh
 # Template specific tests
 # check "distro" lsb_release -c
 # check "color" [ $(cat /tmp/color.txt | grep red) ]
-check "bob --version"
-check "echo $PATH | grep nvim-bin"
-check "nvim -v"
+check "bob is installed" bob --version
+check "bob  binary is in the PATH variable" [ $(echo $PATH | grep nvim-bin) ]
+check "neovim is installed" nvim -v
 
 # Report result
 reportResults
