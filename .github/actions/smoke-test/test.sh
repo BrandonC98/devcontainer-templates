a81dd723ef959afd360b9881b37eaca0e38867fa
Brandon98 <brandoncampbell98@hotmail.co.uk>
2025-06-28 01:01:03 +0000

    add command printing

--- .github/actions/smoke-test/test.sh
+++ .github/actions/smoke-test/test.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 TEMPLATE_ID="$1"
-set -e
+set -ex
 
 SRC_DIR="/tmp/${TEMPLATE_ID}"
 echo "Running Smoke Test"
 
 ID_LABEL="test-container=${TEMPLATE_ID}"
-devcontainer exec --workspace-folder "${SRC_DIR}" --id-label ${ID_LABEL} /bin/bash -c 'set -e && pwd && ls test-project/'
+devcontainer exec --workspace-folder "${SRC_DIR}" --id-label ${ID_LABEL} /bin/bash -c 'set -e && pwd && ls test-project/t '
 devcontainer exec --workspace-folder "${SRC_DIR}" --id-label ${ID_LABEL} /bin/bash -c 'set -e && if [ -f "test-project/test.sh" ]; then cd test-project && if [ "$(id -u)" = "0" ]; then chmod +x test.sh; else sudo chmod +x test.sh; fi && ./test.sh; else ls -a; fi'
 
 # Clean up
